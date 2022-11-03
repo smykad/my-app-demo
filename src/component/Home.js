d6e51af1716d7ae506994ce170f04aa7608f1504
Doug <70054506+smykad@users.noreply.github.com>
2022-11-03 06:37:35 -0400

    clock

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Clock from './Clock'
 
 export const Home = () => {
   return (
-    <div><h1>Home</h1></div>
-  
+    <div><h1>Home</h1>
+    <Clock/>
+    </div> 
   )
 }
