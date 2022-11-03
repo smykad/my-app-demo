f032c0b2e5dce952154e3044401a22e5aafeb331
Doug <70054506+smykad@users.noreply.github.com>
2022-11-03 05:41:17 -0400

    master

---
+++ src/component/Contatct.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Contatct = () => {
+  return (
+    <div><h1>Contatct</h1></div>
+  )
+}
---
+++ src/component/NavbarComp.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { About } from './About';
+import { Contatct } from './Contatct';
+import { Home } from './Home';
+
+
+export default class NavbarComp extends Component {
+  render() {
+    return (
+        <Router>
+        <div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+              <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+              <Nav.Link as={Link} to={"/contact"}>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+      <Routes>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contatct/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </div>          
+      </div>
+      </Router> 
+  
+      
+    )
+  }
+}
