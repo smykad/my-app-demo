a13530415b2813dfe4063b55543486d2a9b24b01
Doug <70054506+smykad@users.noreply.github.com>
2022-11-03 06:45:57 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './component/NavbarComp';
+import Clock from './component/Clock';
 
 function App() {
   return (
     <div className="App">
      <NavbarComp/>
+     <Clock/>
     </div>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Clock from './Clock'
 
 export const Home = () => {
   return (
     <div><h1>Home</h1>
-    <Clock/>
-    </div> 
+    
+    </div>
+
   )
 }
